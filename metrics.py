a8b58757b63c1b0fa687f8a9692644bec607c62a
Jamsheedy <58054056+Jamsheedy@users.noreply.github.com>
2020-05-31 13:23:12 -0400

    How to add a metric:
    -  there are several steps to add a metric to the sheet:
      1) Search for metric column (Line 11)
      2) Get column from sheet as list (Line 20)
      3) Store values for each attribute in their columns (Line 29)
      4) Update attribute columns with updated lists (Line 37)
    
    - note for step 3, you need to define how to calculate your metric
      - Starts at line 45
      - All functions take in the the current row (Y) and the start war
      - All functions in this version deal with war stars
        - traverse through player columns and handle data

--- metrics.py
+++ metrics.py
@@ -2,30 +2,39 @@ from funcs import get_time
 
 
 def update_metrics(sheet):
-    header = sheet.getRow(2)
+    header = sheet.getRow(2)  # row needed to look up the columns
+
     try:
+        # use row to look up column numbers for attributes
         average_stars_col = header.index('AVG Stars') + 1
         war_efficiency_col = header.index('War Efficiency') + 1
+        star_efficiency_col = header.index('Star Efficiency') + 1
         start_war = header.index('Attack 1')
     except ValueError:
-        print(get_time() + ' - ERROR: {AVG Stars} not in sheet row 2!')
+        print(get_time() + ' - ERROR: Column not in sheet row 2!')
         return
 
-    avg_list = sheet.getColumn(average_stars_col)               # columns that are being updated
+    # get the actual columns we need as lists
+    avg_list = sheet.getColumn(average_stars_col)
     wef_list = sheet.getColumn(war_efficiency_col)
+    sef_list = sheet.getColumn(star_efficiency_col)
 
     for i in range(2, len(avg_list)):
-        player_row = sheet.getRow(i + 1)        # get player row from column
+        player_row = sheet.getRow(i + 1)  # get player row from column
 
         # ATTRIBUTES WE ARE STORING
         # note: we only use one api request for n attributes
         avg_list[i] = get_avg_stars(player_row, start_war)
         wef_list[i] = get_war_efficiency(player_row, start_war)
+        sef_list[i] = get_star_efficiency(player_row, start_war)
+
+        # data has been calculated and written to member in list
     # end for loop
 
     # update attribute lists to their respective columns
     sheet.updateColumn(average_stars_col, avg_list)
     sheet.updateColumn(war_efficiency_col, wef_list)
+    sheet.updateColumn(star_efficiency_col, sef_list)
 
     # all metrics up to date
     print(get_time() + ' - Metrics Updated')
@@ -68,3 +77,22 @@ def get_war_efficiency(y_current, start):
         return -1
 
     return round(attacks_used / attacks_available * 100, 2)
+
+
+def get_star_efficiency(y_current, start):
+    stars_earned = 0
+    stars_available = 0
+    for i in range(start, len(y_current)):
+        try:
+            star = int(y_current[i])  # get the cell in player row
+            if 3 >= star >= 0:
+                stars_earned += star
+                stars_available += 3
+            # else is not a star, could be clan games, maybe decimals im not sure
+        except ValueError:  # its a string, need to handle missed attacks('-')
+            if y_current[i] == '-':
+                stars_available += 3
+
+    if stars_available == 0:
+        return -1
+    return round(stars_earned / stars_available * 100, 2)
