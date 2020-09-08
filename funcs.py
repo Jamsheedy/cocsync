599fda174076a47a3de779422e9488c2b9a9edc8
Jamsheedy <58054056+Jamsheedy@users.noreply.github.com>
2020-09-07 23:25:52 -0400

    Summer Update - funcs.py
    Improvements:
     - get_time() returns EST
    Additions:
     - simple_time():
      - returns m/d/y h:m AM/PM EST timestamp for sheet

--- funcs.py
+++ funcs.py
@@ -1,5 +1,8 @@
-from time import gmtime, strftime
+from time import localtime, strftime
 
 def get_time():
-    return strftime("%a, %d %b %Y %H:%M:%S", gmtime())
+    return strftime("%a, %d %b %Y %H:%M:%S", localtime())
+
+def simple_time():
+    return strftime("%m/%d/%Y %I:%M %p", localtime()) + ' EST'
 
